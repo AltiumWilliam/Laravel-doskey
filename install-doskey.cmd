0004c441c1ba13a4b7252f5048ebae7358a84f3e
william Fisher <Altium.ir@Gmail.com>
2019-05-28 19:41:48 +0430

    Update install-doskey.cmd

--- install-doskey.cmd
+++ install-doskey.cmd
@@ -1,6 +1,6 @@
 @echo off
-@echo Copy files to %userprofile% directory.
-@copy laravel-doskeys.cmd %userprofile% /y
-@echo Add new registery value
-@REG ADD "HKCU\Software\Microsoft\Command Processor" /v Autorun /d %userprofile%\laravel-doskeys.cmd /f
+echo Copy files to %userprofile% directory.
+copy laravel-doskeys.cmd %userprofile% /y
+echo Add new registery value
+REG ADD "HKCU\Software\Microsoft\Command Processor" /v Autorun /d %userprofile%\laravel-doskeys.cmd /f
 pause
